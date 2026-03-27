a393bfaa7d6839334b7ab7995253a23f8fa79f5d
notysozu <lifegamer2050@gmail.com>
2026-03-27 15:07:56 +0530

    feat(install.ps1): add dependency installation

--- install.ps1
+++ install.ps1
@@ -30,3 +30,37 @@ function Get-PackageManager {
 
   $Script:PackageManager = 'unknown'
 }
+
+function Install-SystemDependencies {
+  $missing = @()
+
+  if (-not (Get-Command git -ErrorAction SilentlyContinue)) { $missing += 'Git.Git' }
+  if (-not (Get-Command python -ErrorAction SilentlyContinue)) { $missing += 'Python.Python.3.11' }
+  if (-not (Get-Command npm -ErrorAction SilentlyContinue)) { $missing += 'OpenJS.NodeJS' }
+  if (-not (Get-Command openssl -ErrorAction SilentlyContinue)) { $missing += 'ShiningLight.OpenSSL.Light' }
+
+  if ($missing.Count -eq 0) {
+    return
+  }
+
+  switch ($Script:PackageManager) {
+    'winget' {
+      foreach ($package in $missing) {
+        winget install --id $package --accept-source-agreements --accept-package-agreements
+      }
+    }
+    'choco' {
+      foreach ($package in $missing) {
+        switch ($package) {
+          'Git.Git' { choco install git -y }
+          'Python.Python.3.11' { choco install python --version=3.11.9 -y }
+          'OpenJS.NodeJS' { choco install nodejs-lts -y }
+          'ShiningLight.OpenSSL.Light' { choco install openssl.light -y }
+        }
+      }
+    }
+    default {
+      throw 'No supported Windows package manager was found.'
+    }
+  }
+}
