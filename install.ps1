2997b2261b6c9d87fc3d276d0ee8852e35e0b265
notysozu <lifegamer2050@gmail.com>
2026-03-27 15:07:41 +0530

    feat(install.ps1): add Chocolatey/Winget detection

--- install.ps1
+++ install.ps1
@@ -9,9 +9,24 @@ param()
 
 $Script:WindowsVersion = $null
 $Script:WindowsEdition = $null
+$Script:PackageManager = $null
 
 function Get-WindowsDetails {
   $os = Get-CimInstance -ClassName Win32_OperatingSystem
   $Script:WindowsVersion = $os.Version
   $Script:WindowsEdition = $os.Caption
+}
+
+function Get-PackageManager {
+  if (Get-Command winget -ErrorAction SilentlyContinue) {
+    $Script:PackageManager = 'winget'
+    return
+  }
+
+  if (Get-Command choco -ErrorAction SilentlyContinue) {
+    $Script:PackageManager = 'choco'
+    return
+  }
+
+  $Script:PackageManager = 'unknown'
 }
