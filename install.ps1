4a58451f3635f43ee3d9fb3131388c377d0d083c
notysozu <lifegamer2050@gmail.com>
2026-03-27 15:07:27 +0530

    feat(install.ps1): add Windows version detection

--- install.ps1
+++ install.ps1
@@ -6,3 +6,12 @@ Run with:
 #>
 
 param()
+
+$Script:WindowsVersion = $null
+$Script:WindowsEdition = $null
+
+function Get-WindowsDetails {
+  $os = Get-CimInstance -ClassName Win32_OperatingSystem
+  $Script:WindowsVersion = $os.Version
+  $Script:WindowsEdition = $os.Caption
+}
